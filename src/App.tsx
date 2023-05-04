dc492858ec71a9a4a39acbc2e2eed0271b409be7
piyushjha0409 <peeyush0409@gmail.com>
2023-05-04 23:54:08 +0530

    completed the tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React, {FC} from 'react';
+import React, {FC, createContext} from 'react';
 import { Person, HairColor } from './components/Person';
 import './App.css';
+
+const  AppContext =  createContext<AppContextInterface | null>(null)
+
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
 
 const App: FC = () => {
 
@@ -9,6 +17,11 @@ const App: FC = () => {
   const email: string = "peeyush0409@gmail.com"
   const isMale: boolean = true
 
+  const ContextValue: AppContextInterface = {
+    name: "Piyush",
+    age: 20,
+    country: "India"
+  }
   // //function 
   // const getName = (name: string): number => {
   //   if(name == "Piyush"){
@@ -18,9 +31,11 @@ const App: FC = () => {
   //   }
   // }
   return (
+    <AppContext.Provider value={ContextValue}>
     <div className="App">
        <Person name={name} email={email} hairColor={HairColor.Brown}/>
     </div>
+    </AppContext.Provider>
   );
 }
 
